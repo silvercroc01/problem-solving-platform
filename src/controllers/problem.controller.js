dfa6e819eb45389c6d7a95ae8827930c0cf85745
rmridul <mridulraj189@gmail.com>
2025-06-03 15:01:17 +0530

    Add routing structure for versioned api

---
+++ src/controllers/problem.controller.js
@@ -0,0 +1,33 @@
+function pingProblemController(req, res) {
+  return res.json({message:'problem controller up'})
+}
+
+function addProblem(req, res) {
+
+}
+
+function getProblem(req, res) {
+
+}
+
+function getProblems(req, res) {
+
+}
+
+function deleteProblem(req, res) {
+
+}
+
+
+function updateProblem(req, res) {
+
+}
+
+module.exports = {
+  addProblem,
+  getProblem,
+  getProblems,
+  deleteProblem,
+  updateProblem,
+  pingProblemController
+}
