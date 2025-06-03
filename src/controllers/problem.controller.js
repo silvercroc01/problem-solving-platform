aa7d152b804996ab3f5d0d8f052daf9f5e749278
rmridul <mridulraj189@gmail.com>
2025-06-03 19:07:06 +0530

    Add unimplemented error for api contract

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,25 +1,39 @@
 function pingProblemController(req, res) {
-  return res.json({message:'problem controller up'})
+  return res.json({ message: 'problem controller up' })
 }
 
 function addProblem(req, res) {
-
+  return res.status(501).json({
+    message: "not implemented yet"
+  })
 }
 
 function getProblem(req, res) {
+  return res.status(501).json({
+    message: "not implemented yet"
+  })
 
 }
 
 function getProblems(req, res) {
+  return res.status(501).json({
+    message: "not implemented yet"
+  })
 
 }
 
 function deleteProblem(req, res) {
+  return res.status(501).json({
+    message: "not implemented yet"
+  })
 
 }
 
 
 function updateProblem(req, res) {
+  return res.status(501).json({
+    message: "not implemented yet"
+  })
 
 }
 
