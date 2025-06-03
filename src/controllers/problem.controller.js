36bdd959d6dca5ff2e67f2438d9a86ac7b79274a
rmridul <mridulraj189@gmail.com>
2025-06-03 19:14:25 +0530

    Fix: migrated numeric http-codes to http-status-codes

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,29 +1,30 @@
+const { StatusCodes } = require('http-status-codes')
 function pingProblemController(req, res) {
   return res.json({ message: 'problem controller up' })
 }
 
 function addProblem(req, res) {
-  return res.status(501).json({
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
     message: "not implemented yet"
   })
 }
 
 function getProblem(req, res) {
-  return res.status(501).json({
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
     message: "not implemented yet"
   })
 
 }
 
 function getProblems(req, res) {
-  return res.status(501).json({
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
     message: "not implemented yet"
   })
 
 }
 
 function deleteProblem(req, res) {
-  return res.status(501).json({
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
     message: "not implemented yet"
   })
 
@@ -31,7 +32,7 @@ function deleteProblem(req, res) {
 
 
 function updateProblem(req, res) {
-  return res.status(501).json({
+  return res.status(StatusCodes.NOT_IMPLEMENTED).json({
     message: "not implemented yet"
   })
 
